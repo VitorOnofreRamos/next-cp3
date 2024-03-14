510e4ab801c2267298e67c025b828a5bfac3673f
Vitor Onofre Ramos <v1toronofreramos@outlook.com>
2024-03-14 09:31:09 -0300

    Estilização dos Componente Header e Footer

--- src/Components/Header/Index.tsx
+++ src/Components/Header/Index.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+import './styles.css'
+
 const Header = () => {
     return(
         <>
-        <header>
+        <header className='header'>
+            <Image 
+             className='img'
+             src="/icon.png"
+             alt="Youtube icon" 
+             title="Youtube" 
+             width="100" height="100"/>
             <p>Aqui vai o header</p>
         </header>
         </>
